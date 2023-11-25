53ae7d099270e04f3907ccce1492d81838776bb6
Desiignner <jlaba2001@gmail.com>
2023-11-25 17:20:09 +0100

    Some new changes

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<ContactEntity> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"data sourece = d:\contacts.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactEntity>()
+                .HasKey(e => e.ContactId);
+            modelBuilder.Entity<ContactEntity>()
+                .HasData(
+            new ContactEntity()
+            {
+                ContactId = 1,
+                Name = "Test",
+                Email = "Test@mail.pl",
+                Phone = "647859276",
+                Priority = 1,
+
+            },
+            new ContactEntity()
+            {
+                ContactId = 2,
+                Name = "Test1",
+                Email = "Test1@mail.pl",
+                Phone = "647415276",
+                Priority = 3,
+                Birth = new DateTime(2000, 11, 12)
+
+            }
+
+        );
+
+
+        }
+    }
+}
---
+++ Data/Entities/ContactEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    [Table("contacts")]
+    public class ContactEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("name")]
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime? Date { get; set; }
+        //DataTime? = moze byc null
+
+        public int Priority { get; set; }
+        public object? ContactId { get; internal set; }
+        public DateTime Birth { get; internal set; }
+    }
+}
