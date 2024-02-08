697295b680ec0591840e21a86b6ae2036c11e9e4
Desiignner <jlaba2001@gmail.com>
2024-02-08 19:50:50 +0100

    Testy Jednostkowe

---
+++ TestyJednostkowe/UnitTest1.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Laboratorium3.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Laboratorium3Tests.Models
+{
+    [TestClass]
+    public class ComputerTests
+    {
+        [TestMethod]
+        public void Computer_ValidData_ShouldPassValidation()
+        {
+            // Arrange
+            var computer = new Computer
+            {
+                Id = 1,
+                Nazwa = "Komputer Testowy",
+                Procesor = "Testowy Procesor",
+                Memory = 8,
+                Karta_Graficzna = "Testowa Karta Graficzna",
+                Producent = "Testowy Producent",
+                Data_Produkcji = DateTime.Now,
+                Priority = Priority.High // Zak³adam, ¿e masz enum Priority zdefiniowany w modelu
+            };
+
+            // Act
+            var validationContext = new ValidationContext(computer, null, null);
+            var validationResult = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(computer, validationContext, validationResult, true);
+
+            // Assert
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void Computer_InvalidData_ShouldFailValidation()
+        {
+            // Arrange
+            var computer = new Computer
+            {
+                Id = 2,
+                Nazwa = null, // Invalid, as it is required
+                Procesor = "Testowy Procesor",
+                Memory = 8,
+                Karta_Graficzna = "Testowa Karta Graficzna",
+                Producent = "Testowy Producent",
+                Data_Produkcji = DateTime.Now,
+                Priority = (Priority)100 // Invalid, assuming Priority enum does not have value 100
+            };
+
+            // Act
+            var validationContext = new ValidationContext(computer, null, null);
+            var validationResult = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(computer, validationContext, validationResult, true);
+
+            // Assert
+            Assert.IsFalse(isValid);
+            Assert.AreEqual(2, validationResult.Count); // 2 expected validation errors
+        }
+    }
+}
